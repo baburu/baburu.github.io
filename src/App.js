a434a0e2f37272107ed8e2bd3eb2c6a81d869d6b
baburu <63157632+baburu@users.noreply.github.com>
2022-11-20 13:02:47 +0100

    test

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import "./css/research.css";
 import "./css/dpstable.css";
 import Header from "./header";
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Nikke from "./nikke";
 
 function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/nikke" />} />
+        <Route path="/nikke" element={<Nikke />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import Nikke from "./nikke";
-import DPSDestroyers from "./dps_chart_dd";
-import DPSLightCruisers from "./dps_chart_cl";
-import DPSHeavyCruisers from "./dps_chart_ca";
-import DPSAircraftCarriers from "./dps_chart_cv";
-import DPSBattleshipsHE from "./dps_chart_bb_he";
-import DPSBattleshipsAP from "./dps_chart_bb_ap";
-import Research from "./research";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/" element={<Navigate to="/nikke" />} />
-        <Route path="/nikke" element={<Nikke />} />
-      </Route>
+      <Route path="*" element={<App />} />
     </Routes>
   </BrowserRouter>
 );
